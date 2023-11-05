8fb31603a52940742b5989b4414fbe55ecc5b28c
s-115-22 <s-115-22@gumiforest.ae.krakow.pl>
2023-11-05 17:18:04 +0100

    l2

--- 04-Subroutines/15.py
+++ 04-Subroutines/15.py
@@ -3,16 +3,20 @@ def keypad():
     n = 1
     for i in range (3):
         for j in range (3):
-            print(f"{n} ", end="")
+            print(f"{n}", end="")
             n += 1
+            if (j+1)%3:
+                print(f" ", end="")
         print("")
 
 
 def keypad2():
     for i in range(1,10):
-        print(f"{i} ", end="")
+        print(f"{i}", end="")
         if not(i%3): 
             print("")
+        else:
+            print(" ", end="")
 
 
 keypad()
